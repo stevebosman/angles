d28dba3acd7c903018c34524dc598f76e2e4c710
Steve Bosman <stevebosman@gmail.com>
2020-03-01 11:46:09 +0000

    Keep track of which unit is the most accurate (assumes initial unit is the most accurate)

--- src/uk/co/stevebosman/angles/Angle.kt
+++ src/uk/co/stevebosman/angles/Angle.kt
@@ -1,33 +1,71 @@
 package uk.co.stevebosman.angles
 
-class Angle private constructor(val radians:Double, val degrees:Double) {
+class Angle private constructor(val accuracy: Accuracy, val radians: Double, val degrees: Double) {
     operator fun unaryMinus(): Angle {
-        return Angle(-radians, -degrees)
+        return Angle(accuracy, -radians, -degrees)
     }
+
     operator fun plus(v: Angle): Angle {
-        return Angle(radians + v.radians, degrees + v.degrees)
+        return if (accuracy == Accuracy.RADIANS || accuracy != v.accuracy) {
+            fromRadians(radians + v.radians, if (accuracy == v.accuracy) {
+                accuracy
+            } else {
+                Accuracy.MIXED
+            })
+        } else {
+            fromDegrees(degrees + v.degrees)
+        }
     }
 
-    operator fun times(d: Double): Angle {
-        return Angle(radians * d, degrees * d)
+    operator fun minus(v: Angle): Angle {
+        return if (accuracy == Accuracy.RADIANS || accuracy != v.accuracy) {
+            fromRadians(radians - v.radians, if (accuracy == v.accuracy) {
+                accuracy
+            } else {
+                Accuracy.MIXED
+            })
+        } else {
+            fromDegrees(degrees - v.degrees)
+        }
     }
 
-    operator fun div(d: Double): Angle {
-        return Angle(radians / d, degrees / d)
+    operator fun times(d: Number): Angle {
+        return if (accuracy == Accuracy.DEGREES) {
+            fromDegrees(degrees * d.toDouble())
+        } else {
+            fromRadians(radians * d.toDouble(), accuracy)
+        }
+    }
+
+    operator fun div(d: Number): Angle {
+        return if (accuracy == Accuracy.DEGREES) {
+            fromDegrees(degrees / d.toDouble())
+        } else {
+            fromRadians(radians / d.toDouble(), accuracy)
+        }
+    }
+
+    override fun toString(): String {
+        return if (accuracy == Accuracy.DEGREES) "$degrees°" else if (accuracy == Accuracy.MIXED) "~$degrees°/$radians rads" else "$radians rads"
     }
 
     companion object {
-        fun fromRadians(value: Double): Angle {
-            return Angle(value, radToDeg(value))
+        private fun fromRadians(value: Number, accuracy: Accuracy): Angle {
+            return Angle(accuracy, value.toDouble(), radToDeg(value.toDouble()))
         }
-        fun fromDegrees(value: Double): Angle {
-            return Angle(degToRad(value), value)
+
+        fun fromRadians(value: Number): Angle {
+            return fromRadians(value, Accuracy.RADIANS)
         }
-        fun fromDegrees(value: Int, minutes: Double=0.0, seconds: Double=0.0): Angle {
-            val degrees = value + (minutes + seconds/60)/60
-            return Angle(degToRad(degrees), degrees)
+
+        fun fromDegrees(value: Number, minutes: Number = 0.0, seconds: Number = 0.0): Angle {
+            val degrees = value.toDouble() + (minutes.toDouble() + seconds.toDouble() / 60) / 60
+            return Angle(Accuracy.DEGREES, degToRad(degrees), degrees)
         }
     }
 
+    enum class Accuracy {
+        DEGREES, RADIANS, MIXED
+    }
 }
 
---
+++ test/uk/co/stevebosman/angles/AngleTest.kt
@@ -0,0 +1,178 @@
+package uk.co.stevebosman.angles
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import kotlin.math.PI
+
+internal class AngleTest {
+
+    @Test
+    fun from0Degrees() {
+        val test = Angle.fromDegrees(0);
+        assertEquals(0.0, test.radians)
+        assertEquals(0.0, test.degrees)
+    }
+
+    @Test
+    fun from90Degrees() {
+        val test = Angle.fromDegrees(90);
+        assertEquals(PI / 2, test.radians)
+        assertEquals(90.0, test.degrees)
+    }
+
+    @Test
+    fun from180Degrees() {
+        val test = Angle.fromDegrees(180);
+        assertEquals(PI, test.radians)
+        assertEquals(180.0, test.degrees)
+    }
+
+    @Test
+    fun from270Degrees() {
+        val test = Angle.fromDegrees(270);
+        assertEquals(3 * PI / 2, test.radians)
+        assertEquals(270.0, test.degrees)
+    }
+
+    @Test
+    fun from360Degrees() {
+        val test = Angle.fromDegrees(360);
+        assertEquals(2 * PI, test.radians)
+        assertEquals(360.0, test.degrees)
+    }
+
+    @Test
+    fun from0Radians() {
+        val test = Angle.fromRadians(0.0);
+        assertEquals(0.0, test.radians)
+        assertEquals(0.0, test.degrees)
+    }
+
+    @Test
+    fun fromPiOver2Radians() {
+        val test = Angle.fromRadians(PI / 2);
+        assertEquals(PI / 2, test.radians)
+        assertEquals(90.0, test.degrees)
+    }
+
+    @Test
+    fun fromPiRadians() {
+        val test = Angle.fromRadians(PI);
+        assertEquals(PI, test.radians)
+        assertEquals(180.0, test.degrees)
+    }
+
+    @Test
+    fun from3PiOver2Radians() {
+        val test = Angle.fromRadians(3 * PI / 2);
+        assertEquals(3 * PI / 2, test.radians)
+        assertEquals(270.0, test.degrees)
+    }
+
+    @Test
+    fun from2PiRadians() {
+        val test = Angle.fromRadians(2 * PI);
+        assertEquals(2 * PI, test.radians)
+        assertEquals(360.0, test.degrees)
+    }
+
+    @Test
+    fun unaryMinus() {
+        val test = -Angle.fromRadians(PI)
+        assertEquals(-PI, test.radians)
+        assertEquals(-180.0, test.degrees)
+    }
+
+    @Test
+    fun plusDegrees() {
+        val test = Angle.fromDegrees(180.0) + Angle.fromDegrees(90);
+        assertEquals(Angle.Accuracy.DEGREES, test.accuracy)
+        assertEquals(PI * 3 / 2, test.radians)
+        assertEquals(270.0, test.degrees)
+    }
+
+    @Test
+    fun plusRadians() {
+        val test = Angle.fromRadians(PI) + Angle.fromRadians(PI / 2);
+        assertEquals(Angle.Accuracy.RADIANS, test.accuracy)
+        assertEquals(PI * 3 / 2, test.radians)
+        assertEquals(270.0, test.degrees)
+    }
+
+    @Test
+    fun plusMixed() {
+        val test = Angle.fromRadians(PI) + Angle.fromDegrees(90);
+        assertEquals(Angle.Accuracy.MIXED, test.accuracy)
+        assertEquals(PI * 3 / 2, test.radians)
+        assertEquals(270.0, test.degrees)
+    }
+
+    @Test
+    fun minusDegrees() {
+        val test = Angle.fromDegrees(180) - Angle.fromDegrees(90);
+        assertEquals(Angle.Accuracy.DEGREES, test.accuracy)
+        assertEquals(PI / 2, test.radians)
+        assertEquals(90.0, test.degrees)
+    }
+
+    @Test
+    fun minusRadians() {
+        val test = Angle.fromRadians(PI) - Angle.fromRadians(PI / 2);
+        assertEquals(Angle.Accuracy.RADIANS, test.accuracy)
+        assertEquals(PI / 2, test.radians)
+        assertEquals(90.0, test.degrees)
+    }
+
+    @Test
+    fun minusMixed() {
+        val test = Angle.fromRadians(PI) - Angle.fromDegrees(90);
+        assertEquals(Angle.Accuracy.MIXED, test.accuracy)
+        assertEquals(PI / 2, test.radians)
+        assertEquals(90.0, test.degrees)
+    }
+
+    @Test
+    fun timesRadians() {
+        val test = Angle.fromRadians(PI) * 3;
+        assertEquals(Angle.Accuracy.RADIANS, test.accuracy)
+        assertEquals(PI * 3, test.radians)
+        assertEquals(540.0, test.degrees)
+    }
+
+    @Test
+    fun timesDegrees() {
+        val test = Angle.fromDegrees(180) * 3;
+        assertEquals(Angle.Accuracy.DEGREES, test.accuracy)
+        assertEquals(PI * 3, test.radians)
+        assertEquals(540.0, test.degrees)
+    }
+
+    @Test
+    fun timesMixed() {
+        val test = (Angle.fromRadians(PI / 2) + Angle.fromDegrees(90)) * 3;
+        assertEquals(Angle.Accuracy.MIXED, test.accuracy)
+        assertEquals(PI * 3, test.radians)
+        assertEquals(540.0, test.degrees)
+    }
+
+    @Test
+    fun divRadians() {
+        val test = Angle.fromRadians(2 * PI) / 2;
+        assertEquals(PI, test.radians)
+        assertEquals(180.0, test.degrees)
+    }
+
+    @Test
+    fun divDegrees() {
+        val test = Angle.fromDegrees(360) / 2;
+        assertEquals(PI, test.radians)
+        assertEquals(180.0, test.degrees)
+    }
+
+    @Test
+    fun divMixed() {
+        val test = (Angle.fromDegrees(180) + Angle.fromRadians(PI)) / 2;
+        assertEquals(PI, test.radians)
+        assertEquals(180.0, test.degrees)
+    }
+}
