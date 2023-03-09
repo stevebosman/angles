ecdf55250214c6079675cf521d29b24931f12d75
Steve Bosman <stevebosman@gmail.com>
2023-03-09 23:13:53 +0000

    Add missing tests
    Signed-off-by: Steve Bosman <stevebosman@gmail.com>

--- src/test/kotlin/TrigonometryTest.kt
+++ src/test/kotlin/TrigonometryTest.kt
@@ -1,7 +1,10 @@
 package uk.co.stevebosman.angles
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import kotlin.math.*
 import kotlin.math.acos as kacos
 import kotlin.math.asin as kasin
@@ -79,6 +82,12 @@ internal class TrigonometryTest {
         assertEquals(PI, asec(-1.0).radians)
     }
 
+    @ParameterizedTest
+    @ValueSource(doubles = [0.99999, 0.0, -0.99999])
+    fun asecIllegalArgumentException(d: Double) {
+        assertThrows(IllegalArgumentException::class.java) { asec(d) }
+    }
+
     @Test
     fun acsc() {
         assertEquals(PI / 2, acsc(1.0).radians)
@@ -89,10 +98,10 @@ internal class TrigonometryTest {
     fun acot() {
         // test values taken from http://www.maths.surrey.ac.uk/hosted-sites/R.Knott/Fibonacci/simpleTrig.html
         assertEquals(90.0, acot(0).degrees, 2e-14)
-        assertEquals(82.5, acot((sqrt(2.0)-1)*(sqrt(3.0)- sqrt(2.0))).degrees, 2e-14)
-        assertEquals(75.0, acot(2-sqrt(3.0)).degrees, 2e-14)
-        assertEquals(72.0, acot(sqrt(1-(2/sqrt(5.0)))).degrees, 2e-14)
-        assertEquals(67.5, acot(sqrt(2.0)-1).degrees, 2e-14)
-        assertEquals(60.0, acot(1/ sqrt(3.0)).degrees, 2e-14)
+        assertEquals(82.5, acot((sqrt(2.0) - 1) * (sqrt(3.0) - sqrt(2.0))).degrees, 2e-14)
+        assertEquals(75.0, acot(2 - sqrt(3.0)).degrees, 2e-14)
+        assertEquals(72.0, acot(sqrt(1 - (2 / sqrt(5.0)))).degrees, 2e-14)
+        assertEquals(67.5, acot(sqrt(2.0) - 1).degrees, 2e-14)
+        assertEquals(60.0, acot(1 / sqrt(3.0)).degrees, 2e-14)
     }
 }
